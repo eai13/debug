0bd275fd036d2d2615f421ab962bb9934cd93280
Egor Ivanov <eai84199@gmail.com>
2022-01-21 06:37:08 +0700

    va debug implemented

--- debug.h
+++ debug.h
@@ -1,16 +1,16 @@
 #ifndef DEBUGGERSRC_H
 #define DEBUGGERSRC_H
 
-#define ETHERNET_DEBUG
+#define ADD_COLOR_TIMESTAMP
 
-#define DBG_UART huart1
+#define DBG_PORT huart1
 
 #include "usart.h"
 #include "stdint.h"
 
-void print_db(int8_t * str);
-void print_wr(int8_t * str);
-void print_er(int8_t * str);
-void print_in(int8_t * str);
+void print_db(const char * fmt, ...);
+void print_wr(const char * fmt, ...);
+void print_er(const char * fmt, ...);
+void print_in(const char * fmt, ...);
 
 #endif
